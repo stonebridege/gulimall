96c6fedf8702a5809dee3a3623dbf9ff649116aa
stonebridge <stonebridge@njfu.edu.cn>
2021-12-13 23:46:37 +0800

    nacos配置中心

--- mall-coupon/src/main/java/com/stonebridge/mallcoupon/controller/CouponController.java
+++ mall-coupon/src/main/java/com/stonebridge/mallcoupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +26,23 @@ import com.common.utils.Result;
  * @date 2021-12-12 10:50:52
  */
 @RestController
+@RefreshScope
 @RequestMapping("mallcoupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private String age;
+
+
+    @RequestMapping("/test")
+    public Result test() {
+        return Result.ok().put("name", name).put("age", age);
+    }
 
     @RequestMapping("/member/list")
     public Result membercoupons() {
