be574fbdcda55df5552ebd2bd8f688b85fc00d76
stonebridge <stonebridge@njfu.edu.cn>
2022-01-09 15:42:11 +0800

    第三方服务集成aliyun-oss

--- mall-third-party/src/test/java/com/stonebridge/mallthirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/stonebridge/mallthirdparty/MallThirdPartyApplicationTests.java
@@ -1,13 +1,28 @@
 package com.stonebridge.mallthirdparty;
 
+import com.aliyun.oss.OSSClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class MallThirdPartyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    OSSClient ossClient;
 
+    @Test
+    public void testUpload1() throws FileNotFoundException {
+        //上传文件
+        InputStream inputStream = new FileInputStream("C:\\Users\\augt2\\OneDrive\\日常纪录\\Test\\avstriia-ozero-nebo-gory-hdr.jpg");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-ciel", "avstriia-ozero-nebo-gory.jpg", inputStream);
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传完成");
+    }
 }
