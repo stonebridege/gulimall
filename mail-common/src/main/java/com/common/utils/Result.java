9785e63f74baf9a146c67c56523eee2699f22673
叶林 <stonebridge@njfu.edu.cn>
2022-01-19 11:04:42 +0800

    代码格式化

--- mail-common/src/main/java/com/common/utils/Result.java
+++ mail-common/src/main/java/com/common/utils/Result.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
@@ -19,51 +19,52 @@ import java.util.Map;
  * @author Mark sunlightcs@gmail.com
  */
 public class Result extends HashMap<String, Object> {
-	private static final long serialVersionUID = 1L;
-	
-	public Result() {
-		put("code", 0);
-		put("msg", "success");
-	}
-	
-	public static Result error() {
-		return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
-	}
-	
-	public static Result error(String msg) {
-		return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
-	}
-	
-	public static Result error(int code, String msg) {
-		Result r = new Result();
-		r.put("code", code);
-		r.put("msg", msg);
-		return r;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public static Result ok(String msg) {
-		Result r = new Result();
-		r.put("msg", msg);
-		return r;
-	}
-	
-	public static Result ok(Map<String, Object> map) {
-		Result r = new Result();
-		r.putAll(map);
-		return r;
-	}
-	
-	public static Result ok() {
-		return new Result();
-	}
+    public Result() {
+        put("code", 0);
+        put("msg", "success");
+    }
 
-	public Result put(String key, Object value) {
-		super.put(key, value);
-		return this;
-	}
-	public  Integer getCode() {
+    public static Result error() {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
+    }
 
-		return (Integer) this.get("code");
-	}
+    public static Result error(String msg) {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
+    }
+
+    public static Result error(int code, String msg) {
+        Result r = new Result();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static Result ok(String msg) {
+        Result r = new Result();
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static Result ok(Map<String, Object> map) {
+        Result r = new Result();
+        r.putAll(map);
+        return r;
+    }
+
+    public static Result ok() {
+        return new Result();
+    }
+
+    public Result put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+
+    public Integer getCode() {
+
+        return (Integer) this.get("code");
+    }
 
 }
--- mail-common/src/main/java/com/common/vaild/ListValueConstraintValidator.java
+++ mail-common/src/main/java/com/common/vaild/ListValueConstraintValidator.java
@@ -11,24 +11,26 @@ import java.util.Set;
  * 1.第一个泛型指定注解
  * 2.第二个泛型指定要校验什么类型的数据
  */
-public class ListValueConstraintValidator implements ConstraintValidator<ListValue,Integer>{
+public class ListValueConstraintValidator implements ConstraintValidator<ListValue, Integer> {
     //泛型第一个为注解类型，第二个为注解标注的属性的类型
     private Set<Integer> set = new HashSet<>();
+
     //初始化方法
     @Override
     public void initialize(ListValue constraintAnnotation) {
         int[] values = constraintAnnotation.values();
-        for (int value: values
+        for (int value : values
         ) {
             set.add(value);
         }
     }
+
     //判断是否校验成功
     @Override
     public boolean isValid(Integer integer, ConstraintValidatorContext constraintValidatorContext) {
-        if(set.contains(integer)){
+        if (set.contains(integer)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
