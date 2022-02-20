b9173a25c6db2041a9aacbb7959623127aa280fe
stonebridge <stonebridge@njfu.edu.cn>
2022-02-20 18:38:22 +0800

    整个仓库管理系统进nacos注册中心，配置仓库系统网关配置

--- mall-ware/src/main/java/com/stonebridge/mallware/MallWareApplication.java
+++ mall-ware/src/main/java/com/stonebridge/mallware/MallWareApplication.java
@@ -3,7 +3,9 @@ package com.stonebridge.mallware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MallWareApplication {
